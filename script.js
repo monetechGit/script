e1cc5f39143c03c02f436b93dd14cbbc8eebc33b
monetechGit <121112011+monetechGit@users.noreply.github.com>
2023-02-24 14:58:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
+console.log("hey")
 document.getElementById("jenkins-head-icon").setAttribute("src","https://u.jimcdn.com/www400/o/sb13912de7e0b8f6e/userlayout/img/logo-new4.png")
 document.getElementById("jenkins-name-icon").remove()
