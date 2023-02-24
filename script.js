7eba9a9a28e477b75387098ce8281bf0acc6b7f0
monetechGit <121112011+monetechGit@users.noreply.github.com>
2023-02-24 14:55:26 +0100

    create

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById("jenkins-head-icon").setAttribute("src","https://u.jimcdn.com/www400/o/sb13912de7e0b8f6e/userlayout/img/logo-new4.png")
+document.getElementById("jenkins-name-icon").remove()
